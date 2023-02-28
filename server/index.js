a693be65eb84ed10a69de3cdd2dfcfb135c58b91
Ruslan <ruslanmehdiyev922@gmail.com>
2023-03-01 02:11:15 +0400

    in server side was added multer package fot handling file upload

--- server/index.js
+++ server/index.js
@@ -20,41 +20,42 @@ const server = http.createServer(app);
 mongoose.set("strictQuery", true);
 let privateKey = process.env.PRIVATE_KEY;
 
-// app.use((req, res, next) => {
-//   if (
-//     req.url === "/api/auth/login" ||
-//     req.url === "/api/auth/confirm" ||
-//     req.url === "/api/auth/register"
-//   ) {
-//     return next();
-//   }
+app.use((req, res, next) => {
+  if (
+    req.url === "/api/auth/login" ||
+    req.url === "/api/auth/confirm" ||
+    req.url === "/api/auth/register"
+  ) {
+    return next();
+  }
 
-//   let auth = req.headers.authorization?.split(" ");
-//   let token = "";
-//   if (auth) {
-//     if (auth.length === 2) {
-//       token = auth[1];
-//     } else {
-//       return res.status(401).json({ message: "Access Error!" });
-//     }
-//   } else {
-//     return res.status(401).json({ message: "Access Error!" });
-//   }
+  let auth = req.headers.authorization?.split(" ");
+  let token = "";
+  if (auth) {
+    if (auth.length === 2) {
+      token = auth[1];
+    } else {
+      return res.status(401).json({ message: "Access Error!" });
+    }
+  } else {
+    return res.status(401).json({ message: "Access Error!" });
+  }
 
-//   jwt.verify(token, privateKey, function (err, decode) {
-//     if (err) {
-//       return res.status(401).json(err);
-//     } else {
-//       const newToken = jwt.sign({ email: decode.email }, privateKey, {
-//         expiresIn: "5h",
-//       });
-//       res.locals.token = newToken;
-//       next();
-//     }
-//   });
-// });
+  jwt.verify(token, privateKey, function (err, decode) {
+    if (err) {
+      return res.status(401).json(err);
+    } else {
+      const newToken = jwt.sign({ email: decode.email }, privateKey, {
+        expiresIn: "5h",
+      });
+      res.locals.token = newToken;
+      next();
+    }
+  });
+});
 
 // middleware for send a new token in the response header if the original token has expired
+
 app.use((req, res, next) => {
   if (res.locals.token) {
     res.setHeader("Authorization", `Bearer ${res.locals.token}`);
